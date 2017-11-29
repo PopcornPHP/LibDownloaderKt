134cf88ffe95da136f1edd3cdfe0401ab6a3d68b
ReturnedVoid <andrewnech@gmail.com>
2017-11-29 17:15:49 +0200

    class for user`s login response instance

---
+++ app/src/main/java/natus/diit/com/libhelper/model/user/CheckUserLogIn.kt
@@ -0,0 +1,9 @@
+package natus.diit.com.libhelper.model.user
+
+import com.google.gson.annotations.SerializedName
+
+class CheckUserLogIn {
+
+    @SerializedName("response")
+    var user:User? = null
+}
