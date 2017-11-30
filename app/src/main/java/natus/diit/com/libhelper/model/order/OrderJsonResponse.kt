b1c3ead69fb53056fc46910ff0813577bd59af2b
ReturnedVoid <andrewnech@gmail.com>
2017-11-30 18:21:20 +0200

    encapsulated order`s json

---
+++ app/src/main/java/natus/diit/com/libhelper/model/order/OrderJsonResponse.kt
@@ -0,0 +1,8 @@
+package natus.diit.com.libhelper.model.order
+
+import com.google.gson.annotations.SerializedName
+
+class OrderJsonResponse {
+    @SerializedName("response")
+    var response: OrderResponse? = null
+}
