4bd0f9d807cef456b041dc1f5acccc1e36af48f9
ReturnedVoid <andrewnech@gmail.com>
2017-11-30 17:04:21 +0200

    added branchId

--- app/src/main/java/natus/diit/com/libhelper/model/book/Book.kt
+++ app/src/main/java/natus/diit/com/libhelper/model/book/Book.kt
@@ -1,6 +1,9 @@
 package natus.diit.com.libhelper.model.book
 
+import android.support.v7.app.AlertDialog
+import android.text.Html
 import com.google.gson.annotations.SerializedName
+import natus.diit.com.libhelper.model.order.Order
 
 class Book(@field:SerializedName("id")
            var id: Int,
@@ -29,7 +32,9 @@ class Book(@field:SerializedName("id")
            @field:SerializedName("cipher")
            var cipher: String?,
            @field:SerializedName("author_sign")
-           var authorSign: String?
+           var authorSign: String?,
+           @field:SerializedName("branch_id")
+           var branchId: String?
 ) {
     @SerializedName("rel_branch")
     var relBranch: List<Branch>? = null
@@ -50,16 +55,35 @@ class Book(@field:SerializedName("id")
         }
 
     companion object {
-        fun getShortBookName(bookName: String): String {
-            var bn = ""
-            for (z in 0 until bookName.length) {
-                if (bookName[z] != '/') {
-                    bn += bookName[z]
-                } else {
-                    break
-                }
-            }
+        fun getShortBookName(bookName: String?): String? {
+            var bn: String? = bookName ?: return ""
+
+            (0 until bookName.length)
+                    .takeWhile { bookName[it] != '/' }
+                    .forEach { bn += bookName[it] }
             return bn
+        }
+
+        fun showOrderInfo(order: Order?, builder: AlertDialog.Builder) {
+            val bookName = order?.relBook?.name
+            val delPoint = order?.relBook?.relBranch?.get(0)
+
+            if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.N) {
+                builder.setTitle("Інформація про замовлення")
+                        .setMessage(Html.fromHtml("<b>" + "Назва книги: " + "</b>" + bookName
+                                + "<br>" + ("<b>" + "Автори: " + "</b>") + order?.relBook?.authors
+                                + "<br>" + ("<b>" + "Рік: " + "</b>") + order?.relBook?.year
+                                + "<br>" + ("<b>" + "Місце видачі: " + "</b>") + delPoint))
+                        .show()
+            } else {
+                builder.setTitle("Інформація про замовлення")
+                        .setMessage(Html.fromHtml("<b>" + "Назва книги: " + "</b>" + bookName
+                                + "<br>" + ("<b>" + "Автори: " + "</b>") + order?.relBook?.authors
+                                + "<br>" + ("<b>" + "Рік: " + "</b>") + order?.relBook?.year
+                                + "<br>" + ("<b>" + "Місце видачі: " + "</b>") + delPoint,
+                                Html.FROM_HTML_MODE_LEGACY))
+                        .show()
+            }
         }
     }
 }
